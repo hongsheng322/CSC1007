c9fd289c7ebce09dc3b079ad88fd0d956ea04f29
Malvin Tok <93638561+malvintok@users.noreply.github.com>
2022-03-27 20:18:28 +0800

    Add comment to code

--- chardev.c
+++ chardev.c
@@ -1,6 +1,8 @@
-// chardev.c;
-// A simple test of kernel module
+// chardev.c
+// A program to read number of characters
 // To compile, run makefile by entering "make"
+
+// LIBRARIES
 #include <linux/kernel.h> 
 #include <linux/module.h> 
 #include <linux/fs.h>
@@ -14,17 +16,19 @@
 #include <linux/poll.h>
 #include <linux/cdev.h>
 
+// GLOBAL DEFINITIONS
+
 #define DEVICE_NAME "chardev"
 #define SUCCESS 0
 
-//PROTOTYPES
+// FUNCTION PROTOTYPES
 
 static int dev_open(struct inode*, struct file*);
 static int dev_release(struct inode*, struct file*);
 static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
 static ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
 
-//GLOBAL VARIABLES
+// GLOBAL VARIABLES
 
 static struct file_operations fops = {
        .open = dev_open,
@@ -40,68 +44,72 @@ static int sizeOfMessage;
 static int errors;
 static struct class *cls;
  
-/* This function is called when the module is loaded. */ 
+// This function is called when the module is loaded. Initialise the char dev; called by module_init() on line 110
 static int __init chardev_init(void)
 { 
-       major = register_chrdev(0, DEVICE_NAME, &fops);
+       major = register_chrdev(0, DEVICE_NAME, &fops); // Regster and create char dev; Assign major number to device
        if (major < 0)
        {                              
-              printk(KERN_ALERT "CharDev: Module load failed\n");
+              printk(KERN_ALERT "CharDev: Module load failed\n"); // Check for error when loading
               return major;
        }
-       printk(KERN_INFO "CharDev: Loading Module\n"); 
-       printk(KERN_INFO "CharDev: Assigned Major Number %d\n", major);
-       cls = class_create(THIS_MODULE, DEVICE_NAME);
+       printk(KERN_INFO "CharDev: Loading Module\n"); // Activity update
+       printk(KERN_INFO "CharDev: Assigned Major Number %d\n", major); // Activity update
+       cls = class_create(THIS_MODULE, DEVICE_NAME); // create struct class pointer; used in calls to device_create
        device_create(cls, NULL, MKDEV(major,0), NULL, DEVICE_NAME); //create a device driver of DEVICE_NAME with major number
-       pr_info("Device created on /dev/%s\n", DEVICE_NAME);
-       return SUCCESS; 
+       pr_info("Device created on /dev/%s\n", DEVICE_NAME); // Alternative to printk with KERN_INFO; Update activity
+       return SUCCESS; // Return 0; Succesful initialisation
 } 
  
-/* This function is called when the module is removed. */ 
+// This function is called when the module is removed. Called by module_exit() on line 111
 static void __exit chardev_exit(void)
 {  
-       device_destroy(cls, MKDEV(major,0));
-       class_destroy(cls);
+       device_destroy(cls, MKDEV(major,0)); // Unregisters and cleans up device previously created with device_create() on line 59
+       class_destroy(cls); // Destroy the struct class pointer created on line 58
 
-       unregister_chrdev(major, DEVICE_NAME);
-       printk(KERN_INFO "CharDev: Removing Module\n");
+       unregister_chrdev(major, DEVICE_NAME); // Unregister char dev created on line 50
+       printk(KERN_INFO "CharDev: Removing Module\n"); // Update activity
 }
 
-static int dev_open(struct inode *inodep, struct file *filep)
+// Called when device is opened from user space
+static int dev_open(struct inode *inodep, struct file *filep) // Takes the device from down to up state; Ready for use
 {
-       printk(KERN_INFO "CharDev device opened\n");
-       return SUCCESS;
+       printk(KERN_INFO "CharDev device opened\n"); // Update activity
+       return SUCCESS; // Return 0; Successful, interfaced prepared to be used
 }
 
+// Called when data is sent from user space to kernel
 static ssize_t dev_write(struct file *filep, const char *buffer,
                          size_t len, loff_t *offset)
 {
-       writeCount++;
-       printk(KERN_INFO "CharDev is being written %d times\n", writeCount);
-       strcpy(msg, buffer);
-       printk(KERN_INFO "Message from User: %s \nNo. of char: %d\n", msg, strlen(msg));
-       return SUCCESS;
+       writeCount++; // Counter to record number of times function called
+       printk(KERN_INFO "CharDev is being written %d times\n", writeCount); // Update activity
+       strcpy(msg, buffer); // Copy message from msg to buffer
+       printk(KERN_INFO "Message from User: %s \nNo. of char: %d\n", msg, strlen(msg)); // Update activity
+       return SUCCESS; // Return 0
 }
 
+// Called when device is closed in user space
 static int dev_release(struct inode *inodep, struct file *filep)
 {
-       printk(KERN_INFO "CharDev device closed\n");
-       return SUCCESS;
+       printk(KERN_INFO "CharDev device closed\n"); // Update activity
+       return SUCCESS; // Return 0
 }
 
+// Called when data sent from device to user space
 static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset)
 {
-       readCount++;
-       printk(KERN_INFO "CharDev is being read %d times\n", readCount);
-       sizeOfMessage = strlen(msg) + 1; //include terminating null
-       printk(KERN_INFO "Copying %s size of %d bytes to User Space\n", msg, sizeOfMessage);
-       errors = copy_to_user(buffer, &msg, sizeOfMessage); //copy to user program the length of message
-       return SUCCESS;
+       readCount++; // Record number of times function called
+       printk(KERN_INFO "CharDev is being read %d times\n", readCount); // Update activity
+       sizeOfMessage = strlen(msg) + 1; // Length of message, including terminating null
+       printk(KERN_INFO "Copying %s size of %d bytes to User Space\n", msg, sizeOfMessage); // Update activity
+       errors = copy_to_user(buffer, &msg, sizeOfMessage); // Copy to user program the length of message
+       return SUCCESS; // Return 0
 }
 
-module_init(chardev_init);
-module_exit(chardev_exit);
+module_init(chardev_init); // chardev_init function (on line 48) to be called at module insertion time
+module_exit(chardev_exit); // chardev_exit function (on line 65) to be called at module removal time
  
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("CharDev Module");
-MODULE_AUTHOR("CSC1007 Group 6");
+MODULE_LICENSE("GPL"); // GNU General Public License; GPL Free Software
+MODULE_DESCRIPTION("CharDev Module"); // Describes what the module is
+MODULE_AUTHOR("CSC1007 Group 6"); // Let others know who the author of this module is
--- chardevclient.c
+++ chardevclient.c
@@ -12,64 +12,66 @@ int main() {
     char *msg = (char*)malloc(SIZE * sizeof(char));
     char c[SIZE];
 
-    printf("Welcome to CSC1007 User to Kernel Program!\n");
+    printf("Welcome to CSC1007 User to Kernel Program!\n"); // Welcome message on command line
 
-    //Opening driver
-    printf("Opening driver...");
-    fd = open("/dev/chardev", O_RDWR);  //open the driver
-    if (fd == -1)                     //if fail to open the driver (the drive is not in kernel)
+    // OPEN DRIVER
+    printf("Opening driver..."); // Update activity on command line
+    fd = open("/dev/chardev", O_RDWR); // Open the device driver at location /dev/chardev
+    if (fd == -1) //if fail to open the driver (the driver is not in kernel; not loaded)
     {
-        perror("open /dev/chardev");
-        printf("Failed!\n");
-        exit(EXIT_FAILURE);
+        perror("open /dev/chardev"); // POSIX error function; Print message to stderr
+        printf("Failed!\n"); // Update user of failure on command line
+        exit(EXIT_FAILURE); // Exit with EXIT_FAILURE=1
     }
-    printf("Success!\n");
+    printf("Success!\n"); // Print if no error; Update user of success on command line
 
-    //Get user input
-    printf("Please enter your message: ");
-    if (fgets(msg, SIZE, stdin) == NULL){
-        exit(EXIT_FAILURE);
+    // GET USER INPUT
+    printf("Please enter your message: "); // Asks for user input on command line
+    if (fgets(msg, SIZE, stdin) == NULL){ // Check for null input by user; Cannot create pointer to strign buffer
+        exit(EXIT_FAILURE); // Exit with EXIT_FAILURE=1
     }
-    msg[strcspn(msg, "\n")] = 0;        //remove /n from input
-    printf("\nYou entered the message: \"%s\" which is %d char long\n", msg, strlen(msg));
+    msg[strcspn(msg, "\n")] = 0; //remove \n from input; Remove terminating null
+    printf("\nYou entered the message: \"%s\" which is %d char long\n", msg, strlen(msg)); // Update user of message length on command line
 
-    //Writing to driver
-    printf("Attempting to write to driver...");
-    rtn = write(fd, msg, strlen(msg));   //write to driver systemcall
-    if (rtn < 0)
+    // WRITING TO DEVICE DRIVER
+    printf("Attempting to write to driver..."); // Update user of writing to driver on command line
+    rtn = write(fd, msg, strlen(msg)); // System call for writing to device driver
+    if (rtn < 0) // Check for error when writing to driver
     {
-        printf(" Failed!\n", rtn);
-        perror("WRITE ERROR:");
+        printf(" Failed!\n", rtn); // Update user on error with error code from write() on command line
+        perror("WRITE ERROR:"); // POSIX error function; Print message to stderr
     }
     else
     {
-        printf(" Success!\n");
+        printf(" Success!\n"); // Update user on successful write to driver on command line
     }
 
-    //Wait for user input
-    printf("\n\nPress \"ENTER\" to continue...");
+    // PAUSE THE PROGRAM
+    printf("\n\nPress \"ENTER\" to continue..."); // Wait for user input before continuing with reading from driver
     char temp;
-    fflush(stdin);
+    fflush(stdin); // Flush output buffer; Clean up stream
     do{
-        temp = fgetc(stdin);
-        if (temp == 0x0A){
-            break;
+        temp = fgetc(stdin); // Check standard in
+        if (temp == 0x0A){ // Until \n or end of line
+            break; // Then break
         }
-    } while(1);
+    } while(1); // If not keep checking standard in
 
-    //Reading from driver
-    printf("\nAttempting to read from driver...");
-    rtn = read(fd, &c, SIZE);            //read driver systemcall
-    if (rtn < 0)
+    // READING FROM DRIVER
+    printf("\nAttempting to read from driver..."); // Update user of reading from driver on command line
+    rtn = read(fd, &c, SIZE); // System call to read from driver
+    if (rtn < 0) // Check for error when reading from driver
     {
-        printf(" Failed!\n");
-        perror("READ ERROR:");
+        printf(" Failed!\n", rtn); // Update user on error with error code from read() on command line
+        perror("READ ERROR:"); // POSIX error function; Print message to stderr
     } 
     else 
     {
-        printf(" Success!\n");
-        printf("Message from kernel space: \"%s\" which is %d char long\n", c, strlen(c));
-    }
+        printf(" Success!\n"); // Update user on successful read on command line
+        printf("Message from kernel space: \"%s\" which is %d char long\n", c, strlen(c)); // // Update user of message length on command line
+    } 
+    // User can check if what is written and what is read matches. 
+    // This proves that the read / write to and from user application device and kernel is accurate.
 
-    exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS); // Exit with EXIT_SUCCESS=0
 }
